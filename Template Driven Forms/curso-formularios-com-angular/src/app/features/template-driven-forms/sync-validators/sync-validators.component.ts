628447c6796bc44ab3ef60e23d05f8173ae430eb
farleybsd <farley.ti@hotmail.com>
2026-06-09 15:54:05 -0300

    .
    .

--- Template Driven Forms/curso-formularios-com-angular/src/app/features/template-driven-forms/sync-validators/sync-validators.component.ts
+++ Template Driven Forms/curso-formularios-com-angular/src/app/features/template-driven-forms/sync-validators/sync-validators.component.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IncludeWordValidatorDirective } from './validators/include-word-validator.directive';
 
 @Component({
   selector: 'app-sync-validators',
-  imports: [],
+  imports: [FormsModule, JsonPipe, IncludeWordValidatorDirective],
   templateUrl: './sync-validators.component.html',
   styleUrl: './sync-validators.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SyncValidatorsComponent {}
+export class SyncValidatorsComponent {
+
+  protected message = signal('');
+
+  protected includeWord = signal('carro');
+
+}
