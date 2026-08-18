0b455ee65f78e36dd4b27479b8faee7205359e48
farleybsd <farley.ti@hotmail.com>
2026-08-18 20:57:16 -0300

    Utilizando funções para forçar estado

--- Template Driven Forms/curso-formularios-com-angular/src/app/features/reactive-forms/routes.ts
+++ Template Driven Forms/curso-formularios-com-angular/src/app/features/reactive-forms/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FormControlComponent } from './1.form-control/form-control.component';
 import { ValidationsComponent } from './2.validations/validations/validations.component';
+import { UsingMarkFuncionsComponent } from './3.using-mark-funcions/using-mark-funcions.component';
 
 export const reactiveFormsRoutes: Routes = [
   {
@@ -10,5 +11,9 @@ export const reactiveFormsRoutes: Routes = [
   {
     path: 'validations',
     component: ValidationsComponent
+  },
+   {
+    path: 'using-mark-funcions',
+    component: UsingMarkFuncionsComponent
   },
 ];
